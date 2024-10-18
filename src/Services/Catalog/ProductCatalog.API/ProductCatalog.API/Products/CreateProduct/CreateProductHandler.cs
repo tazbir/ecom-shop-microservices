f6abc9a308159c6dc8abbfd0a3f2978d296f3062
Tazbir <tazbir@brainstation-23.com>
2024-10-18 11:52:30 +0600

    catagory api is ready

---
+++ src/Services/Catalog/ProductCatalog.API/ProductCatalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,29 @@
+using Base.CQRS;
+using ProductCatalog.API.Models;
+
+namespace ProductCatalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, string Description, List<string> Category, decimal Price, string ImageFile)
+    : ICommand<CreateProductResult>;
+
+public record CreateProductResult(Guid Id);
+
+public class CreateProductHandler: ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        Product product = new()
+        {
+            Id = Guid.NewGuid(),
+            Name = command.Name,
+            Description = command.Description,
+            Price = command.Price,
+            ImageFile = command.ImageFile,
+            Category = command.Category
+        };
+        
+        //TODO: Save product to database
+        
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
--- src/Services/Catalog/ProductCatalog.API/ProductCatalog.API/Program.cs
+++ src/Services/Catalog/ProductCatalog.API/ProductCatalog.API/Program.cs
@@ -1,9 +1,13 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // add services to the container
+builder.Services.AddCarter();
+builder.Services.AddMediatR(config =>
+    config.RegisterServicesFromAssemblies(typeof(Program).Assembly));
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline
+app.MapCarter();
 
 app.Run();
